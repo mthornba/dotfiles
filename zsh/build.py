8877e2b1801b4788d6566e1da3df6258b9fff0c8
mthornba <matt.thornback@gmail.com>
2023-01-16 17:27:14 -0500

    feat: move os detection to build script

--- zsh/build.py
+++ zsh/build.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-import sys, jinja2
+import sys, platform
+import jinja2
 
 templateLoader = jinja2.FileSystemLoader(searchpath="./src")
-templateEnv = jinja2.Environment(loader=templateLoader)
+templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)
+os = platform.system()
+template_vars = { "os": os }
 
 for file in sys.argv[1:]:
   TEMPLATE_FILE = file.lstrip(".") + ".j2"
   template = templateEnv.get_template(TEMPLATE_FILE)
-  outputText = template.render()
+  outputText = template.render(template_vars)
 
   f = open(file, "w")
   f.write(outputText)
