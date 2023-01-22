14d5e7026c9684bddf901d12615caaab5a6b9754
mthornba <matt.thornback@gmail.com>
2023-01-22 15:55:46 -0500

    refactor: zsh tests

--- test/test_stow.py
+++ test/test_stow.py
@@ -1,16 +1,29 @@
 import os
 
-homedir = os.path.expanduser("~")
+cwd = os.getcwd()
+
+def pathExists(filename):
+  fqFilename = os.path.expanduser(filename)
+  return os.path.exists(fqFilename)
+
+def pathIsLink(filename):
+  fqFilename = os.path.expanduser(filename)
+  return os.path.islink(fqFilename)
 
 class TestZsh:
   def test_zshrc(self):
-    filename = '.zshrc'
-    fqFilename = os.path.join(homedir,filename)
-    assert os.path.exists(fqFilename) == True
-    assert os.path.islink(fqFilename) == True
+    assert pathIsLink('~/.zshrc') == True
 
   def test_zshenv(self):
-    filename = '.zshenv'
-    fqFilename = os.path.join(homedir,filename)
-    assert os.path.exists(fqFilename) == True
-    assert os.path.islink(fqFilename) == True
+    filename = '~/.zshenv'
+    assert pathIsLink(filename) == True
+
+  def test_not_exist(self):
+    stowPath = '~'
+    filesExist = [ '.zshrc', '.zshenv' ]
+    filesNoExist = os.listdir(os.path.join(cwd,'zsh'))
+    for file in filesExist:
+      filesNoExist.remove(file)
+
+    for filename in filesNoExist:
+      assert not pathExists(os.path.join(stowPath, filename)) == True
