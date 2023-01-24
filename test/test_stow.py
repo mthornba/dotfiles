8b8da1b2c9b46b6ac0f616281b1fb5cc16b2e3cd
mthornba <matt.thornback@gmail.com>
2023-01-24 09:54:05 -0500

    refactor: zsh tests and add starship tests

--- test/test_stow.py
+++ test/test_stow.py
@@ -3,6 +3,13 @@ import os, pytest
 cwd = os.getcwd()
 stowPath = '~'
 
+def lsRecursive(lsRoot):
+  return list(
+    os.path.join(parent, name)
+    for (parent, subdirs, files) in os.walk(lsRoot)
+    for name in files + subdirs
+  )
+
 def pathExists(filename):
   fqFilename = os.path.expanduser(filename)
   return os.path.exists(fqFilename)
@@ -11,16 +18,38 @@ def pathIsLink(filename):
   fqFilename = os.path.expanduser(filename)
   return os.path.islink(fqFilename)
 
+def pathIsDir(filename):
+  fqFilename = os.path.expanduser(filename)
+  return os.path.isdir(fqFilename)
+
 class TestZsh:
-  filesExist = [ '.zshrc', '.zshenv' ]
-  filesNoExist = os.listdir(os.path.join(cwd,'zsh'))
-  for filename in filesExist:
-    filesNoExist.remove(filename)
+  filesAll = list(map(lambda x: x.removeprefix('zsh/'), lsRecursive('zsh')))
+  filesExist = list(['.zshrc', '.zshenv'])
+  filesNoExist = [ x for x in filesAll if x not in list(['.zshrc', '.zshenv']) ]
 
   @pytest.mark.parametrize("filename", filesExist)
   @pytest.mark.parametrize("stowPath", stowPath)
   def test_exists(self, stowPath, filename):
-    assert pathIsLink(os.path.join(stowPath, filename)) == True
+    isLink = pathIsLink(os.path.join(stowPath, filename))
+    isDir = pathIsDir(os.path.join(stowPath, filename))
+    assert ( isLink or isDir ) == True
+
+  @pytest.mark.parametrize("filename", filesNoExist)
+  @pytest.mark.parametrize("stowPath", stowPath)
+  def test_not_exist(self, stowPath, filename):
+    assert not pathExists(os.path.join(stowPath, filename)) == True
+
+class TestStarship:
+  filesAll = list(map(lambda x: x.removeprefix('starship/'), lsRecursive('starship')))
+  filesExist = list(['.config', '.config/starship.toml'])
+  filesNoExist = [ x for x in filesAll if x not in list(['.config', '.config/starship.toml']) ]
+
+  @pytest.mark.parametrize("filename", filesExist)
+  @pytest.mark.parametrize("stowPath", stowPath)
+  def test_exists(self, stowPath, filename):
+    isLink = pathIsLink(os.path.join(stowPath, filename))
+    isDir = pathIsDir(os.path.join(stowPath, filename))
+    assert ( isLink or isDir ) == True
 
   @pytest.mark.parametrize("filename", filesNoExist)
   @pytest.mark.parametrize("stowPath", stowPath)
